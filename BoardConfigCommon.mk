DevelopedInside/android_device_nubia_msm8998-common

msm8998-common: Enforce vendor tree presence
A compiled build has no chance of booting without the prebuilt
binaries, so fail the build if those are not present.

Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

BoardConfigCommon.mk
@@ -296,4 +296,4 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
--include vendor/nubia/msm8998-common/BoardConfigVendor.mk
+include vendor/nubia/msm8998-common/BoardConfigVendor.mk

ProjectElixir-Devices/device_oneplus_msm8998-common

Change-Id: I8124a5f92a75490ce48422b9d760764733efe37e

BoardConfigCommon.mk
@@ -168,4 +168,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
--include vendor/oneplus/msm8998-common/BoardConfigVendor.mk
+include vendor/oneplus/msm8998-common/BoardConfigVendor.mk